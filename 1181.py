a2fe63c56eaefbb307608aebfe678cab52b1e2f0
sinokadev <sinoka@sinoka.dev>
2026-03-23 22:24:07 +0900

    edit

--- 1181.py
+++ 1181.py
@@ -1,22 +1,14 @@
 length = int(input())
 MAX_LEN = 50
 words = []
-words_index = [[] for _ in range(MAX_LEN)]
 
 for _ in range(length):
     inp = input()
-    if inp in words:
-        continue
     words.append(inp)
-    words_index[len(inp)-1].append(inp)
 
-for i in range(MAX_LEN):
-    words_index[i].sort()
+words_two = list(set(words))
 
+words_two.sort(key=lambda x: (len(x), x))
 
-print("---")
-
-for i in words_index:
-    for j in i:
-        if j:
-            print(j)
+for i in words_two:
+    print(i)
