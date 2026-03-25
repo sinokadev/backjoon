7a260f8b2ef7eac8cd43e171ad3e5fc23e22f56c
sinokadev <sinoka@sinoka.dev>
2026-03-25 15:07:55 +0900

    9498

---
+++ 9498.py
@@ -0,0 +1,12 @@
+number = int(input())
+
+if 100 >= number >= 90:
+    print("A")
+elif 89 >= number >= 80:
+    print("B")
+elif 79 >= number >= 70:
+    print("C")
+elif 69 >= number >= 60:
+    print("D")
+else:
+    print("F")
