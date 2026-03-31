4e745821abbf68af8e0024ba317e4fdf77fbddf9
sinokadev <sinoka@sinoka.dev>
2026-03-31 13:08:43 +0900

    162888, 34412, 34446

---
+++ 34412.py
@@ -0,0 +1,6 @@
+floor_number = int(input())
+
+if floor_number >= 13:
+    print(floor_number+1)
+else:
+    print(floor_number)
---
+++ 34446.py
@@ -0,0 +1,5 @@
+mile = int(input())
+students = int(input())
+temperature = int(input())
+
+print(mile*2)
