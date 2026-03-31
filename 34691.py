2901862c2613f567a4b683c55f7c17d674e5d2ca
sinokadev <sinoka@sinoka.dev>
2026-03-31 19:43:55 +0900

    34691

---
+++ 34691.py
@@ -0,0 +1,8 @@
+while (i := input()) != "end" :
+    match i:
+        case "animal":
+            print("Panthera tigris")
+        case "tree":
+            print("Pinus densiflora")
+        case "flower":
+            print("Forsythia koreana")
