4688b749ba6f3efb8b63f120eeaeddc668e4770a
sinokadev <sinoka@sinoka.dev>
2026-04-22 12:26:51 +0900

    31614, 31654, 32314, 32384, 32498

---
+++ 32314.py
@@ -0,0 +1,7 @@
+a = int(input())
+w, v = input().split()
+
+if int(w) // int(v) >= a:
+    print(1)
+else:
+    print(0)
---
+++ 32498.py
@@ -0,0 +1,7 @@
+a = 0
+
+for i in range(int(input())):
+    if int(input())%2 != 0:
+        a += 1
+
+print(a)
