864b2419a1c4411b03531c68ee77a5e5e3027d0a
sinokadev <sinoka@sinoka.dev>
2026-04-25 23:15:34 +0900

    34362, 34411, 34454, 34921

---
+++ 34454.py
@@ -0,0 +1,7 @@
+n = int(input())
+c = int(input())
+p = int(input())
+if (c*p) >= n:
+    print("yes")
+else:
+    print("no")
---
+++ 34921.py
@@ -0,0 +1,5 @@
+a, t = input().split()
+
+p = 10+2*(25-int(a)+int(t))
+
+print(max(0, p))
